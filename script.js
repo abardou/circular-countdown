9bed965bab5e6c80480f99843640a9d00837892b
Anthony Bardou <a.bardou@outlook.fr>
2020-07-28 12:08:56 +0200

    Date in parameters

--- script.js
+++ script.js
@@ -39,23 +39,41 @@ function secondsToTimeString(seconds, units, values, total) {
 	return str
 }
 
+var getUrlParameter = function getUrlParameter(sParam) {
+	var sPageURL = window.location.search.substring(1),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+					return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+			}
+	}
+};
+
 var c = null;
 var radius = null;
+var ss_crit = 600;
+var ss_fs = 0.065
+var nss_fs = 0.047
 
 $(document).ready(function() {
 	var h = $(document).height()
 	var w = $(document).width()
-	var dim = 0.98 * Math.min(h, w)
+	var dim = 0.99 * Math.min(h, w)
 	$('.item').css({'width':dim+'px', 'height':dim+'px'});
-	var stroke_width = dim * 0.045;
+	var stroke_width = 0.05 * dim;
 	c = dim / 2
 	radius = c - stroke_width;
 	var diam = radius * 2;
-	var font_size = diam * 0.045
+	var font_size = dim > ss_crit ? diam * nss_fs : diam * ss_fs;
 	$('.item').append("<h2 style=\"width: "+(0.85*diam)+"px; font-size: "+ font_size +"px\"></h2><svg width=\""+dim+"px\" height=\""+dim+"px\"><path id=\"arc1\" fill=\"none\" stroke=\"#fa691d\" stroke-width=\""+stroke_width+"\"/></svg>")
 
-	let start = new Date('2020-07-24T08:20:00');
-	let end = new Date('2020-08-16T11:05:00');
+	let start = new Date(getUrlParameter('s')); // &s=2020-07-24T08%3A20%3A00&e=2020-08-16T11%3A05%3A00
+	let end = new Date(getUrlParameter('e'));
 	let whole_delay = (end - start) / 1000;
 	units = ['Week', 'Day', 'Hour', 'Minute', 'Second'];
 	values = [604800, 86400, 3600, 60, 1];
@@ -75,13 +93,13 @@ $(window).resize(function() {
 	console.log("Document resized")
 	h = $(document).height()
 	w = $(document).width()
-	dim = 0.95 * Math.min(h, w)
+	dim = 0.99 * Math.min(h, w)
 	$('.item').css({'width':dim+'px', 'height':dim+'px'});
-	stroke_width = dim * 0.045;
+	stroke_width = 0.05 * dim;
 	c = dim / 2
 	radius = c - stroke_width;
 	diam = radius * 2;
-	font_size = diam * 0.045
+	font_size = dim > ss_crit ? diam * nss_fs : diam * ss_fs
 	$('h2').css({'width': 0.85*diam+'px', 'font-size': font_size+'px'});
 	$('svg').css({'width': dim+'px', 'height': dim+'px'})
 	document.getElementById("arc1").setAttribute("stroke-width", stroke_width);
